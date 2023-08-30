7202710a516d3f5005db066fff255a668bc390b0
marcuser01 <essential.living.sdnbhd@gmail.com>
2023-08-31 00:55:27 +0800

    Completes exercise 01-03 without problem

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let splitChars = string.split('');
+  let reversedString = '';
+  for (let i = splitChars.length - 1; i >= 0; i--) {
+    reversedString += splitChars[i];
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
